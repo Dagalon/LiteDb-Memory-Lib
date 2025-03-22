d1d56bc8456180f08117c888391c4debc7460bfc
Dagalon <63908589+Dagalon@users.noreply.github.com>
2025-03-22 19:03:48 +0100

    First commit

---
+++ LiteDb-Memory-Lib/ConnectionManager.cs
@@ -0,0 +1,38 @@
+﻿using LiteDB;
+
+namespace LiteDb_Memory_Lib;
+
+public sealed class ConnectionManager
+{
+    #region Atrributes
+    private static ConnectionManager? _instance;
+    private static Dictionary<string, LiteDatabase> _databases = new();
+    #endregion
+    
+    #region Contructors
+
+    private ConnectionManager()
+    {
+        _databases = new Dictionary<string, LiteDatabase>();
+    }
+
+    #endregion
+    
+    #region Methods
+
+    public static LiteDatabase? GetDatabase(string alias, bool createIfNotExists = false)
+    {
+        if (_databases.TryGetValue(alias, out LiteDatabase? value))
+        {
+            return value;
+        }
+
+        if (!createIfNotExists) return null;
+       
+        _databases.Add(alias, new LiteDatabase(":memory:"));
+        return _databases[alias];
+
+    }
+
+    #endregion
+}
