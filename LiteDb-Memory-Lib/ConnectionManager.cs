7016aa649ad1212463057e26cc916f333734bced
Dagalon <63908589+Dagalon@users.noreply.github.com>
2025-03-29 14:12:28 +0100

    First steps

--- LiteDb-Memory-Lib/ConnectionManager.cs
+++ LiteDb-Memory-Lib/ConnectionManager.cs
@@ -5,16 +5,16 @@ namespace LiteDb_Memory_Lib;
 public sealed class ConnectionManager
 {
     #region Atrributes
+    
     private static ConnectionManager? _instance;
     private static Dictionary<string, LiteDatabase> _databases = new();
+    private static Dictionary<string, MemoryStream> _memoryFiles = new();
+    
     #endregion
     
     #region Contructors
 
-    private ConnectionManager()
-    {
-        _databases = new Dictionary<string, LiteDatabase>();
-    }
+    private ConnectionManager() { }
 
     #endregion
     
@@ -34,5 +34,51 @@ public sealed class ConnectionManager
 
     }
 
+    public static void CreateDatabase(string alias, string path, bool substituteIfExist = false, bool isShared = false)
+    {
+
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            if (_databases.ContainsKey(alias))
+            {
+                throw new Exception($"There is a data base in memory with alias {alias}");
+            } 
+            
+            _memoryFiles.Add(alias, new MemoryStream());
+            _databases.Add(alias, new LiteDatabase(new MemoryStream()));
+        }
+        else
+        {
+            if (File.Exists(path))
+            {
+                var connectionString = isShared 
+                    ? @$"filename={path};connection=shared"  
+                    : @$"filename={path};connection=direct";
+                
+                if (_databases.ContainsKey(alias))
+                {
+                    if (substituteIfExist)
+                    {
+                        _databases.Remove(alias);
+                        _databases.Add(alias, new LiteDatabase(new ConnectionString(connectionString)));
+                    }
+                    else
+                    {
+                        throw new Exception($"The database already {alias} exists.");
+                    }
+                }
+                else
+                {
+                    _databases.Add(alias, new LiteDatabase(new ConnectionString(connectionString)));
+                }
+            }
+            else
+            {
+                throw new Exception($"The path {path} does not exist.");
+            }
+
+        }
+    }
+    
     #endregion
 }
