3bc8dd82cc72be8cee3bd8d6cf8fe90299c1c438
Dagalon <63908589+Dagalon@users.noreply.github.com>
2025-06-30 21:17:46 +0200

    New method to execute general query

---
+++ LiteDb-Memory-Tests/T_Execute_Queries.cs
@@ -0,0 +1,46 @@
+﻿using LiteDb_Memory_Lib;
+using LiteDB;
+
+namespace LiteDb_Memory_Tests;
+
+public class ExecuteQueries
+{
+    
+    public struct Ticket
+    {
+        public ObjectId OrderId { get; set; }
+        public string ObjectType {get; set; }
+        public int Amount { get; set; }
+    }
+
+    
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+    [Test]
+    public void T_Get_Collections()
+    {
+        var idDataBase = "TEST_DB";
+        var manager = ConnectionManager.Instance();
+        
+        // Create database
+        manager.CreateDatabase(idDataBase);
+        
+        // Create collection
+        var food = new List<Ticket>()
+        {
+            new() {OrderId = ObjectId.NewObjectId(), ObjectType = "Banana", Amount = 6},
+            new() {OrderId = ObjectId.NewObjectId(), ObjectType = "Orange", Amount = 10},
+            new() {OrderId = ObjectId.NewObjectId(), ObjectType = "Apple", Amount = 20}
+        };
+        
+        manager.CreateCollection(idDataBase, "DeliveryList", food);
+        
+        // Query 
+        var qry = @"SELECT $.ObjectType, $.Amount FROM DeliveryList";
+        var result = GeneralTools.Execute<Ticket>(manager, idDataBase, qry);
+
+    }
+}
