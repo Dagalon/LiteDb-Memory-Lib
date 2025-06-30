1520faaa00d427de30135b09c021cd6e4e4f6702
Dagalon <63908589+Dagalon@users.noreply.github.com>
2025-06-30 17:40:33 +0200

    New methods for filtering linked collections

--- LiteDb-Memory-Lib/FilterTools.cs
+++ LiteDb-Memory-Lib/FilterTools.cs
@@ -11,10 +11,24 @@ public static class FilterTools
          return collection is not null ? collection.FindOne(predicate) : default;
     }
     
+    public static T? FindOne<T, TOutput>(ConnectionManager manager, string alias, string collectionName, Expression<Func<T, bool>> predicate,
+        Expression<Func<T, TOutput>> refFunctional)
+    {
+        var collection = manager.GetCollection<T>(alias,collectionName);
+        return collection is not null ? collection.Include(refFunctional).FindOne(predicate) : default;
+    }
+    
     public static T? FindOne<T>(ConnectionManager manager, string alias, string collectionName, BsonExpression qry)
     {
         var collection = manager.GetCollection<T>(alias,collectionName);
         return collection is not null ? collection.FindOne(qry) : default;
+    }
+    
+    public static T? FindOne<T, TOutput>(ConnectionManager manager, string alias, string collectionName, BsonExpression qry, 
+        Expression<Func<T, TOutput>> refFunctional)
+    {
+        var collection = manager.GetCollection<T>(alias,collectionName);
+        return collection is not null ? collection.Include(refFunctional).FindOne(qry) : default;
     }
     
     public static T? FindOne<T>(ConnectionManager manager, string alias, string collectionName, Query qry)
@@ -23,16 +37,38 @@ public static class FilterTools
         return collection is not null ? collection.FindOne(qry) : default;
     }
     
-    public static List<T>? Find<T>(ConnectionManager manager, string alias, string collectionName, Query qry)
+    public static T? FindOne<T, TOutput>(ConnectionManager manager, string alias, string collectionName, Query qry, 
+        Expression<Func<T, TOutput>> refFunctional)
     {
         var collection = manager.GetCollection<T>(alias,collectionName);
-        return collection?.Find(qry).ToList();
+        return collection is not null ? collection.Include(refFunctional).FindOne(qry) : default;
     }
     
-    public static List<T>? Find<T>(ConnectionManager manager, string alias, string collectionName, BsonExpression qry)
+    public static List<T>? Find<T, TOutput>(ConnectionManager manager, string alias, string collectionName, 
+        Expression<Func<T, bool>> predicate, Expression<Func<T, TOutput>> refFunctional)
     {
         var collection = manager.GetCollection<T>(alias,collectionName);
-        return collection?.Find(qry).ToList();
+        return collection?.Include(refFunctional).Find(predicate).ToList();
+    }
+    
+    public static List<T>? Find<T>(ConnectionManager manager, string alias, string collectionName, Expression<Func<T, bool>> predicate)
+    {
+        var collection = manager.GetCollection<T>(alias,collectionName);
+        return collection?.Find(predicate).ToList();
+    }
+    
+    public static List<T>? Find<T, TOutput>(ConnectionManager manager, string alias, string collectionName, Query qry, 
+        Expression<Func<T, TOutput>> refFunctional)
+    {
+        var collection = manager.GetCollection<T>(alias,collectionName);
+        return collection?.Include(refFunctional).Find(qry).ToList();
+    }
+    
+    public static List<T>? Find<T,TOutput>(ConnectionManager manager, string alias, string collectionName, BsonExpression qry,
+        Expression<Func<T, TOutput>> refFunctional)
+    {
+        var collection = manager.GetCollection<T>(alias,collectionName);
+        return collection?.Include(refFunctional).Find(qry).ToList();
     }
     
     public static T? FindById<T>(ConnectionManager manager, string alias, string collectionName, BsonValue id)
@@ -48,7 +84,14 @@ public static class FilterTools
         return collection is null ? default : collection.Include(refFunctional).FindById(id);
     }
     
-    public static List<T>? FindAll<T>(ConnectionManager manager, string alias, string collectionName, BsonValue id)
+    public static List<T>? FindAll<T, TOutput>(ConnectionManager manager, string alias, string collectionName,
+        Expression<Func<T,TOutput>> refFunctional)
+    {
+        var collection = manager.GetCollection<T>(alias,collectionName);
+        return collection?.Include(refFunctional).FindAll().ToList();
+    }
+    
+    public static List<T>? FindAll<T>(ConnectionManager manager, string alias, string collectionName)
     {
         var collection = manager.GetCollection<T>(alias,collectionName);
         return collection?.FindAll().ToList();
