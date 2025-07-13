72b2f6d3b82b1ddcda8081c557aa74b6f1159c47
Dagalon <63908589+Dagalon@users.noreply.github.com>
2025-07-13 12:10:19 +0200

    New tool to keep file in collection FileStorage

---
+++ LiteDb-Memory-Lib/FileStorageTools.cs
@@ -0,0 +1,26 @@
+﻿namespace LiteDb_Memory_Lib;
+
+public static class FileStorageTools
+{
+    public static EnumsLiteDbMemory.Output Upload(ConnectionManager manager, string alias, string idFile,
+        string pathFile)
+    {
+        if (!File.Exists(pathFile))
+        {
+            return EnumsLiteDbMemory.Output.PATH_NOT_FOUND;
+        }
+        
+        var db = manager.GetDatabase(alias);
+
+        if (db is null)
+        {
+            return EnumsLiteDbMemory.Output.DB_NOT_FOUND;
+        }
+        
+        var fs = db.FileStorage;
+        fs.Upload(idFile, pathFile);
+
+        return EnumsLiteDbMemory.Output.SUCCESS;
+    }
+
+}
